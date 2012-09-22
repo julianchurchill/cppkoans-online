32ebb047c65b595fda179a73d01ea088fd0cff35
Julian Churchill <chewielouie03@gmail.com>
2012-09-22 19:34:55 +0100

    Created initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CppkoansOnline::Application.config.secret_token = '8f0c873bdd44f0604035372f434663077de39c3390414cf4a933138d5210e989389e82653c26779cb251a01110e8c02932dbb8ce82fd1070486e6810c9d73155'
